8f0b847e78ed07934bb998aa5223ad1ebafaf3e7
Supriya Garg <supriyagarg@google.com>
2016-10-05 17:41:59 -0400

    Add treemap support to the Groups class.
    Now users can display the groups as a treemap, optionally passing in a
    metric type and time interval to add color to the treemap.

--- datalab/stackdriver/monitoring/_group.py
+++ datalab/stackdriver/monitoring/_group.py
@@ -86,3 +86,109 @@ class Groups(object):
           parent_display_name, group.is_cluster, group.filter])
 
     return pandas.DataFrame(data, columns=self._DISPLAY_HEADERS)
+
+  def _group_display_id(self, group_id):
+    """Creates the ID to display for a group."""
+    group = self._group_dict.get(group_id)
+    if group is None:
+      return group_id
+    return '%s (%s)' % (group.display_name, group_id)
+
+  def treemap(self, metric_type=None,
+              interval=None, end_time=None, days=0, hours=0, minutes=0):
+    """Draws a treemap with the group hierarchy in the project.
+
+    When specifying the time interval for the metric data to read, you can
+    specify only one of `interval` or the `end_time` with
+    `days`/`hours`/`minutes`. By default, the last one hour is selected.
+
+    Args:
+      metric_type: An optional metric type that assigns color to the treemap
+          cells. You can only specify metrics with numeric data.
+      interval: Time interval over which to get the aggregate (mean) of the
+        metric data. For example `TimeInterval.TODAY`. Defaults to None.
+      end_time: The end time (inclusive) of the time interval as a datetime
+        object. The default is the start of the current minute.
+      days: The number of days in the time interval.
+      hours: The number of hours in the time interval.
+      minutes: The number of minutes in the time interval.
+
+    Returns:
+      The HTML rendering of a treemap.
+
+    Raises:
+      ValueError: The time interval specified is not valid.
+    """
+    import datalab.utils.commands
+
+    if (interval is None and end_time is None and
+        not (days or hours or minutes)):
+      hours = 1
+
+    dataframe = self._hierarchy(
+        metric_type, interval=interval,
+        end_time=end_time, days=days, hours=hours, minutes=minutes)
+    dataframe = dataframe.fillna(0)
+    properties = dict(height=500, maxPostDepth=1,
+                      minColor='green', maxColor='red')
+    if metric_type is not None:
+      properties['midColor'] = 'yellow'
+
+    prop = '\n'.join('%s: %s' % (key, value)
+                     for key, value in properties.iteritems())
+    return datalab.utils.commands._chart._chart_cell({
+        'chart': 'treemap',
+        'data': dataframe,
+        'fields': ','.join(dataframe.columns)
+    }, prop)
+
+  def _hierarchy(self, metric_type=None,
+                 interval=None, end_time=None, days=0, hours=0, minutes=0):
+    """Returns a dataframe with the group hierarchy of the project. """
+    _Node = collections.namedtuple(
+        '_Node', ('entity_id', 'parent_id', 'size', 'metric'))
+
+    query = None
+    if metric_type is not None:
+      query = self._build_query(
+          metric_type, interval=interval,
+          end_time=end_time, days=days, hours=hours, minutes=minutes)
+
+    hierarchy_rows = [_Node(entity_id=self._project_id, parent_id=None,
+                            size=0, metric=0)]
+    for group in self.list():
+      parent_id = self._group_display_id(group.parent_id) or self._project_id
+      entity_id = self._group_display_id(group.id)
+      if metric_type is not None:
+        dataframe = query.select_group(group.id).as_dataframe()
+        metric = dataframe.mean().mean()
+      else:
+        metric = 0
+      hierarchy_rows.append(
+          _Node(entity_id=entity_id, parent_id=parent_id, size=1,
+                metric=metric))
+
+    return pandas.DataFrame.from_records(hierarchy_rows, columns=_Node._fields)
+
+  def _build_query(self, metric_type,
+                   interval=None, end_time=None, days=0, hours=0, minutes=0):
+    """Builds a query object based on the metric_type and time interval."""
+    from . import _query
+
+    descriptor = self._client.fetch_metric_descriptor(metric_type)
+    if descriptor.value_type not in ['INT64', 'DOUBLE']:
+      raise ValueError('Only numeric metric types are supported')
+
+    if descriptor.metric_kind == 'CUMULATIVE':
+      alignment_method = 'ALIGN_DELTA'
+    else:
+      alignment_method = 'ALIGN_MEAN'
+
+    query = _query.Query(
+        metric_type, interval=interval, end_time=end_time, days=days,
+        hours=hours, minutes=minutes, project_id=self._project_id,
+        context=self._context)
+    duration = (query._end_time - query._start_time).total_seconds()
+    query = query.align(alignment_method, seconds=int(duration))
+    query = query.reduce('REDUCE_MEAN', 'resource.project_id')
+    return query
